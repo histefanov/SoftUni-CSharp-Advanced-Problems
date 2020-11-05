1607e0cf2ae1cf113bf148803eb717bc6ac2da29
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-11-06 00:02:23 +0200

    add two greedy algorithms

---
+++ Basic Algorithms/SetCover/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetCover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var universe = Console.ReadLine()
+                .Substring(10)
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+            int numberOfSets = int.Parse((Console.ReadLine().Last().ToString()));
+            
+            var setList = new List<int[]>();
+            var resultSets = new List<int[]>();
+
+            for (int i = 0; i < numberOfSets; i++)
+            {
+                int[] currentSet = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                setList.Add(currentSet);
+            }
+            
+            while (universe.Count > 0)
+            {
+                var currentSet = setList.OrderByDescending(s => s.Count(universe.Contains))
+                    .FirstOrDefault();
+                setList.Remove(currentSet);
+                resultSets.Add(currentSet);
+                foreach (var item in currentSet)
+                {
+                    universe.Remove(item);
+                }
+            }
+
+            Console.WriteLine($"Sets to take ({resultSets.Count}):");
+            foreach (var set in resultSets)
+            {
+                var setString = String.Join(", ", set);
+                Console.WriteLine($"{{ {setString} }}");
+            }
+        }
+    }
+}
---
+++ Basic Algorithms/SumOfCoins/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace SumOfCoins
+{
+    public class SumOfCoins
+    {
+        static void Main(string[] args)
+        {
+            int[] availableCoins = Console.ReadLine()
+                .Substring(7)
+                .Split(", ")
+                .Select(int.Parse)
+                .OrderByDescending(c => c)
+                .ToArray();
+            int desiredSum = int.Parse(Console.ReadLine().Substring(5));
+            int[] coinsCount = new int[availableCoins.Length];
+
+            for (int i = 0; i < availableCoins.Length; i++)
+            {
+                coinsCount[i] += desiredSum / availableCoins[i];
+                desiredSum -= coinsCount[i] * availableCoins[i];
+            }
+
+            if (desiredSum > 0)
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"Number of coins to take: {coinsCount.Aggregate((acc, next) => acc + next)}");
+                for (int i = 0; i < availableCoins.Length; i++)
+                {
+                    if (coinsCount[i] > 0)
+                    {
+                        Console.WriteLine($"{coinsCount[i]} coin(s) with value {availableCoins[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
